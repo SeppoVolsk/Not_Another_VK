999d6f7a34ed7eb5be1d01568df908110ce73f89
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-06-15 20:51:09 +0300

    MainScreenRep refactoring in progress

--- lib/presentation/blocs/main_screen_repository.dart
+++ lib/presentation/blocs/main_screen_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:vk_postman/data/api_clients/vk_api_client.dart';
 import 'package:vk_postman/data/data_providers/history_data_provider.dart';
-import 'package:vk_postman/data/data_providers/posts_data_provider.dart';
 import 'package:vk_postman/data/persistent_storage.dart';
 import 'package:vk_postman/domain/entities/full_original_post/full_original_post.dart';
 import 'package:vk_postman/domain/entities/post.dart';
@@ -9,28 +9,50 @@ import 'package:vk_postman/presentation/blocs/mainscreenentity.dart';
 import 'package:vk_postman/presentation/widgets/error_snack_bar.dart';
 
 class IMainScreenRepository {
-  final history = HistoryDataProvider();
-  final storage = PersistentStorage();
+  final _history = HistoryDataProvider();
+  final _storage = PersistentStorage();
+
+  List<Post>? _posts;
+  Map<String, dynamic>? _json;
+  String? _newsQuery;
+
+  void _fetchJson() {}
+  void _convertJsonToPosts() {}
+
+  MainScreenEntity read({required String? query}) =>
+      _templateMethod(query: query);
+
+  MainScreenEntity _templateMethod({required String? query}) {
+    try {
+      final json = _fetchJson();
+    } catch (e) {
+      errorSnackBar(
+          'Не удалось загрузить посты. Повторите попытку или воспользуйтесь поиском');
+      return MainScreenEntity();
+    }
+    _convertJsonToPosts();
+    return MainScreenEntity(
+        posts: _posts, history: _history, newsQuery: _newsQuery);
+  }
 
   MainScreenEntity readSavedPosts(String? storageKey) {
     String? storageJsonString;
     String? newsQuery;
-
     final Map<String, dynamic> json;
     List<Post> posts = [];
 
     if (storageKey == null) {
-      final allStorageKeys = storage.keys;
+      final allStorageKeys = _storage.keys;
       if (allStorageKeys == null) {
         errorSnackBar('Воспользуйтесь поиском');
         return MainScreenEntity();
       }
       newsQuery = allStorageKeys.last;
-      history.historyWords
-          .addAll(allStorageKeys.take(history.maxLength).toList());
-      storageJsonString = storage.read(key: allStorageKeys.last);
+      _history.historyWords
+          .addAll(allStorageKeys.take(_history.maxLength).toList());
+      storageJsonString = _storage.read(key: allStorageKeys.last);
     } else {
-      storageJsonString = storage.read(key: storageKey);
+      storageJsonString = _storage.read(key: storageKey);
     }
 
     if (storageJsonString != null) {
@@ -46,19 +68,14 @@ class IMainScreenRepository {
         return MainScreenEntity();
       }
     }
-    print('readSavedPosts ${history.historyWords}');
+    print('readSavedPosts ${_history.historyWords}');
     return MainScreenEntity(
-      posts: posts,
-      history: history,
-      newsQuery: newsQuery,
-      loadingInProgress: false,
-    );
+        posts: posts, history: _history, newsQuery: newsQuery);
   }
 
   Future<MainScreenEntity> updatePostsFromNet(String newsQuery) async {
     Map<String, dynamic> json;
     List<Post> posts = [];
-    //final history = HistoryDataProvider();
 
     try {
       json = await VkApiClient().getPosts(newsQuery);
@@ -86,12 +103,47 @@ class IMainScreenRepository {
       return MainScreenEntity();
     }
 
-    history.markInHistory(newsQuery: newsQuery, json: json);
+    _history.markInHistory(newsQuery: newsQuery, json: json);
 
     return MainScreenEntity(
-        posts: posts,
-        history: history,
-        newsQuery: newsQuery,
-        loadingInProgress: false);
+        posts: posts, history: _history, newsQuery: newsQuery);
+  }
+}
+
+class MainScreenStoragePosts extends IMainScreenRepository {
+  @override
+  MainScreenEntity read({required String? query}) {
+    final keys = _storage.keys;
+
+    if (query == null && keys == null) {
+      errorSnackBar('История поиска не найдена.Воспользуйтесь поиском');
+      return MainScreenEntity();
+    }
+    if (keys != null) {
+      _initHistory(words: keys);
+    }
+
+    return super.read(query: query);
+  }
+
+  @override
+  _fetchJson() {
+    final lastKey = _storage.keys?.last;
+    q;
+    if (lastKey != null) {
+      _json = jsonDecode(_storage.read(key: lastKey).toString());
+    }
+  }
+
+  @override
+  void _convertJsonToPosts() {
+    int jsonFromStorageLength = (_json?['response']['items']).length;
+    for (int i = 0; i < jsonFromStorageLength; i++) {
+      _posts?.add(Post.postFromJson(json, i));
+    }
+  }
+
+  void _initHistory({required Set<String> words}) {
+    _history.historyWords.addAll(words.take(_history.maxLength).toList());
   }
 }
