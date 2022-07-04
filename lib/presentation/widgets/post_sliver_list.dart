a32ea4d153973a32f6872013542ae4d86868c158
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-07-04 20:47:38 +0300

    HistoryWidget now persistent pinned after Appbar

--- lib/presentation/widgets/post_sliver_list.dart
+++ lib/presentation/widgets/post_sliver_list.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:vk_postman/presentation/widgets/history_widget.dart';
 
 class PostSliverList extends StatefulWidget {
   PostSliverList({Key? key}) : super(key: key);
@@ -16,14 +18,21 @@ class _PostSliverListState extends State<PostSliverList> {
       slivers: [
         //Image.asset('pringles-logo.png'),
         SliverAppBar(
-          title: Text('Sliver App Bar'),
-          backgroundColor: Colors.blueGrey,
+          pinned: true,
+
+          //title: Text('Sliver App Bar'),
+          backgroundColor: Colors.grey,
           flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
             'lib/assets/pringles-logo.png',
             fit: BoxFit.cover,
           )),
           expandedHeight: 300,
+        ),
+        SliverPersistentHeader(
+          delegate: MainScreenPersistentDelegate(),
+          pinned: true,
+          floating: false,
         ),
         SliverList(
             delegate: SliverChildBuilderDelegate((context, index) {
@@ -38,5 +47,26 @@ class _PostSliverListState extends State<PostSliverList> {
         }, childCount: 254))
       ],
     );
+  }
+}
+
+class MainScreenPersistentDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return HistoryWidget();
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => 96;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => 96;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
